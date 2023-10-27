7ba7a439666f9e4c4392eb3bbe535a94ff7d6c77
Michael Swan <10856729+RMichaelSwan@users.noreply.github.com>
2023-10-27 12:19:09 -0700

    Full system info

--- power.bash
+++ power.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Testing Idle for 1 minute. Press Ctrl+c when 1 minute done!"
-nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory,memory.used,memory.free,power.draw --format=csv -l 1 -f gpu_idle.csv
-echo "Testing power run for 1 minute. Press Ctrl+c when 1 minute done!"
-./gpu_burn 60 & stress --cpu 16 --io 4 --vm 2 --vm-bytes 128M --timeout 60 & nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory,memory.used,memory.free,power.draw --format=csv -l 1 -f gpu_high.csv 
+nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory,memory.used,memory.free,power.draw --format=csv -l 1 -f gpu_idle.csv && echo "gpu_idle.csv written to file."
+echo "Testing full power run (CPU+GPU) for 1 minute. Press Ctrl+c when 1 minute done!"
+./gpu_burn 60 & stress --cpu 16 --io 4 --vm 2 --vm-bytes 128M --timeout 60 & nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory,memory.used,memory.free,power.draw --format=csv -l 1 -f gpu_high.csv  && echo "gpu_high.csv written to file."
+echo "Tests complete, getting system info..."
+lsb_release -a > os-info.txt && echo "Wrote os-info.txt to file"
+lscpu > cpu-info.txt && echo "Wrote cpu-info.txt to file"
+lshw > hw-info.txt && echo "Wrote hw-info.txt to file"
+echo "Done!"
